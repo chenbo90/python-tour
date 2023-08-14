973732c5c188e6638a64c72b9ae074d3b839f5eb
chenbo3 <chenbo3@asiainfo.com>
2023-08-14 17:05:45 +0800

    add pipeline code

--- model/pipeline.py
+++ model/pipeline.py
@@ -1,6 +1,7 @@
 from transformers import pipeline
 # 运行该段代码要保障你的电脑能够上网，会自动下载预训练模型，大概420M
 unmasker = pipeline("fill-mask",model = "bert-base-uncased")  # 这里引入了一个任务叫fill-mask，该任务使用了base的bert模型
-unmasker("The goal of life is [MASK].", top_k=5) # 输出mask的指，对应排名最前面的5个，也可以设置其他数字
-# 输出结果如下，似乎都不怎么有效哈。
+l = unmasker("The goal of life is [MASK].", top_k=5)
+print(l)
+
 
