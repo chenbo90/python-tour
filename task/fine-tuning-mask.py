645f78d873a4934992456768d6a8154b52a83f64
chenbo3 <chenbo3@asiainfo.com>
2023-08-17 14:34:13 +0800

    新增微调一个掩码模型-添加数据集-update

--- task/fine-tuning-mask.py
+++ task/fine-tuning-mask.py
@@ -29,6 +29,7 @@ for token in top_5_tokens:
     print(f"'>>> {text.replace(tokenizer.mask_token, tokenizer.decode([token]))}'")
 
 '''
+output:
 '>>> This is a great deal.'
 '>>> This is a great success.'
 '>>> This is a great adventure.'
@@ -39,7 +40,30 @@ for token in top_5_tokens:
 from datasets import load_dataset
 
 imdb_dataset = load_dataset("imdb")
-imdb_dataset
+print(imdb_dataset)
 
 
+'''
+随机打印看看
+seed：随机种子，这可以使随机性的行为在不同运行中保持一致
+'''
+sample = imdb_dataset["train"].shuffle(seed=42).select(range(3))
+
+for row in sample:
+    print(f"\n'>>> Review: {row['text']}'")
+    print(f"'>>> Label: {row['label']}'")
+
+def tokenize_function(examples):
+    result = tokenizer(examples["text"])
+    print("result:"+result)
+    if tokenizer.is_fast:
+        result["word_ids"] = [result.word_ids(i) for i in range(len(result["input_ids"]))]
+    return result
+
+
+# Use batched=True to activate fast multithreading!
+tokenized_datasets = imdb_dataset.map(
+    tokenize_function, batched=True, remove_columns=["text", "label"]
+)
+print(tokenized_datasets)
 
