5bcae38a608ffbddb34411938beb2c7112c9a9d1
chenbo3 <chenbo3@asiainfo.com>
2023-08-17 15:07:53 +0800

    新增微调一个掩码模型-添加数据集-update1

--- task/fine-tuning-mask.py
+++ task/fine-tuning-mask.py
@@ -39,8 +39,30 @@ output:
 
 from datasets import load_dataset
 
-imdb_dataset = load_dataset("imdb")
+#第一种方式：从hub上传下载
+#imdb_dataset = load_dataset("imdb")
+
+#第二种方式，从本地缓存加载
+imdb_dataset = load_dataset("imdb",cache_dir='/home/ubuntu/.cache/huggingface/datasets')
 print(imdb_dataset)
+
+'''
+output:
+DatasetDict({
+    train: Dataset({
+        features: ['text', 'label'],
+        num_rows: 25000
+    })
+    test: Dataset({
+        features: ['text', 'label'],
+        num_rows: 25000
+    })
+    unsupervised: Dataset({
+        features: ['text', 'label'],
+        num_rows: 50000
+    })
+})
+'''
 
 
 '''
@@ -52,6 +74,18 @@ sample = imdb_dataset["train"].shuffle(seed=42).select(range(3))
 for row in sample:
     print(f"\n'>>> Review: {row['text']}'")
     print(f"'>>> Label: {row['label']}'")
+
+'''
+out:
+'>>> Review: There is no relation at all between Fortier and Profiler but the fact that both are police series about violent crimes. Profiler looks crispy, Fortier looks classic. Profiler plots are quite simple. Fortier's plot are far more complicated... Fortier looks more like Prime Suspect, if we have to spot similarities... The main character is weak and weirdo, but have "clairvoyance". People like to compare, to judge, to evaluate. How about just enjoying? Funny thing too, people writing Fortier looks American but, on the other hand, arguing they prefer American series (!!!). Maybe it's the language, or the spirit, but I think this series is more English than American. By the way, the actors are really good and funny. The acting is not superficial at all...'
+'>>> Label: 1'
+
+'>>> Review: This movie is a great. The plot is very true to the book which is a classic written by Mark Twain. The movie starts of with a scene where Hank sings a song with a bunch of kids called "when you stub your toe on the moon" It reminds me of Sinatra's song High Hopes, it is fun and inspirational. The Music is great throughout and my favorite song is sung by the King, Hank (bing Crosby) and Sir "Saggy" Sagamore. OVerall a great family movie or even a great Date movie. This is a movie you can watch over and over again. The princess played by Rhonda Fleming is gorgeous. I love this movie!! If you liked Danny Kaye in the Court Jester then you will definitely like this movie.'
+'>>> Label: 1'
+
+'>>> Review: George P. Cosmatos' "Rambo: First Blood Part II" is pure wish-fulfillment. The United States clearly didn't win the war in Vietnam. They caused damage to this country beyond the imaginable and this movie continues the fairy story of the oh-so innocent soldiers. The only bad guys were the leaders of the nation, who made this war happen. The character of Rambo is perfect to notice this. He is extremely patriotic, bemoans that US-Americans didn't appreciate and celebrate the achievements of the single soldier, but has nothing but distrust for leading officers and politicians. Like every film that defends the war (e.g. "We Were Soldiers") also this one avoids the need to give a comprehensible reason for the engagement in South Asia. And for that matter also the reason for every single US-American soldier that was there. Instead, Rambo gets to take revenge for the wounds of a whole nation. It would have been better to work on how to deal with the memories, rather than suppressing them. "Do we get to win this time?" Yes, you do.'
+'>>> Label: 0'
+'''
 
 def tokenize_function(examples):
     result = tokenizer(examples["text"])
@@ -66,4 +100,36 @@ tokenized_datasets = imdb_dataset.map(
     tokenize_function, batched=True, remove_columns=["text", "label"]
 )
 print(tokenized_datasets)
+
+'''
+out:
+DatasetDict({                                                                                                                  
+    train: Dataset({
+        features: ['input_ids', 'attention_mask', 'word_ids'],
+        num_rows: 25000
+    })
+    test: Dataset({
+        features: ['input_ids', 'attention_mask', 'word_ids'],
+        num_rows: 25000
+    })
+    unsupervised: Dataset({
+        features: ['input_ids', 'attention_mask', 'word_ids'],
+        num_rows: 50000
+    })
+})
+'''
+
+#检查标记器的model_max_length 属性，
+length = tokenizer.model_max_length
+print(length)
+
+chunk_size = 128
+
+# Slicing produces a list of lists for each feature
+tokenized_samples = tokenized_datasets["train"][:3]
+
+for idx, sample in enumerate(tokenized_samples["input_ids"]):
+    print(f"'>>> Review {idx} length: {len(sample)}'")
+
+
 
