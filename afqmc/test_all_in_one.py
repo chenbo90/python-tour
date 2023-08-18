d00163e444e97efd3a74b79fec0006c103ed9244
chenbo3 <chenbo3@asiainfo.com>
2023-08-18 17:22:42 +0800

    update 111

--- afqmc/test_all_in_one.py
+++ afqmc/test_all_in_one.py
@@ -1,15 +1,9 @@
-# from all_in_one import test_loop
 import all_in_one as ai
 import torch
-from transformers import AutoConfig,AutoTokenizer
-# checkpoint = "bert-base-chinese"
-# tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-#
-# device = 'cuda' if torch.cuda.is_available() else 'cpu'
-# print(f'Using {device} device')
 
-# config = AutoConfig.from_pretrained(checkpoint)
-# model = BertForPairwiseCLS.from_pretrained(checkpoint, config=config).to(device)
 
-ai.model.load_state_dict(torch.load('epoch_3_valid_acc_73.9_model_weights.bin'))
+load = torch.load('epoch_3_valid_acc_73.9_model_weights.bin')
+print(load)
+state_dict = ai.model.load_state_dict(load)
+print(state_dict)
 ai.test_loop(ai.valid_dataloader, ai.model, mode='Test')
