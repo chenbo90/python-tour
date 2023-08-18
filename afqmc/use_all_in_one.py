5471802e760c185901d2cac9903310e60c6dc5bd
chenbo3 <chenbo3@asiainfo.com>
2023-08-18 17:12:25 +0800

    update 14

--- afqmc/use_all_in_one.py
+++ afqmc/use_all_in_one.py
@@ -11,5 +11,5 @@ print(f'Using {device} device')
 # config = AutoConfig.from_pretrained(checkpoint)
 # model = BertForPairwiseCLS.from_pretrained(checkpoint, config=config).to(device)
 
-ai.model.load_state_dict(torch.load('epoch_3_valid_acc_74.1_model_weights.bin'))
+ai.model.load_state_dict(torch.load('epoch_3_valid_acc_73.9_model_weights.bin'))
 ai.test_loop(valid_dataloader, ai.model, mode='Test')
